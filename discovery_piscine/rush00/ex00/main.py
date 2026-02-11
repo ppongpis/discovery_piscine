9e14efdfbf08b44cba0a9a0168d350a058151aa4
popongpi <67070253@kmitl.ac.th>
2026-02-11 19:54:33 +0700

    Rush

---
+++ discovery_piscine/rush00/ex00/main.py
@@ -0,0 +1,20 @@
+from checkmate import checkmate
+
+#board
+def main():
+    board = """\
+. . . . . . . .
+. . . . . . . .
+. . . . . . . .
+. . . . . . . .
+. . . . . . . .
+. . . . . . . .
+. . . . . . . .
+. . . . . . . .
+"""
+
+    checkmate(board)
+
+#เชื่อม
+if __name__ == "__main__":
+    main()
